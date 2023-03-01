8f607ecb4e1a4b7e84aa1c6bda3756320048ca62
maxamedjayte <maxamedltc@gmail.com>
2023-03-01 23:42:12 +0300

    df

--- landApp/models.py
+++ landApp/models.py
@@ -14,6 +14,7 @@ class UserProfile(models.Model):
     number=models.CharField(max_length=255)
     username=models.CharField(max_length=255)
     status=models.BooleanField(default=True)
+    # axmad
 
 
 class BorderRegistration(models.Model):
